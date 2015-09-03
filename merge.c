8c283b562142a65fedc4ee73b98316af45c928ae
Pooshan <pooshan.vyas@gmail.com>
2015-09-03 16:37:51 -0700

    Merge sort

---
+++ merge.c
@@ -0,0 +1,48 @@
+//implementation of merge sort
+#include<stdio.h>
+
+int a[20],b[20],n;
+
+void merging(int low,int mid,int high)
+{
+  int l1,l2,i;
+
+  for(l1=low,l2=mid+1,i=low;l1<=mid && l2<=high;i++)
+  if(a[l1]<=a[l2])
+   b[i]=a[l1++];
+  else
+   b[i]=a[l2++];
+
+   while(l1<=mid)    b[i++]=a[l1++];
+   while(l2<=high)   b[i++]=a[l2++];
+
+   for(i=low;i<=high;i++)
+   	a[i]=b[i];
+}
+
+ void sort(int low,int high)
+ {
+    int mid;
+    if(low<high)
+	    {
+	      mid=(low+high)/2;
+	      sort(low,mid);
+	      sort(mid+1,high);
+	      merging(low,mid,high);
+	     }
+    else
+	return;
+ }
+
+  main()
+ { int i;
+    printf("Enter N "); scanf("%d",&n);
+    
+    printf("Enter elements\n");
+    for(i=1;i<=n;i++) scanf("%d",&a[i]);
+    
+	sort(1,n);
+    
+	printf("After sorting\n");
+    for(i=1;i<=n;i++) printf("%d\n",a[i]);
+ }
